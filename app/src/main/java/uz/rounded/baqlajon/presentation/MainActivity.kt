53f905cbcb24b7780ad251d406cb38fa982e16a9
SuhrobDev <suhrobqayumov4@gmail.com>
2023-06-17 12:33:53 +0500

    initial commit

--- app/src/main/java/uz/rounded/baqlajon/presentation/MainActivity.kt
+++ app/src/main/java/uz/rounded/baqlajon/presentation/MainActivity.kt
@@ -35,7 +35,6 @@ class MainActivity : AppCompatActivity() {
         R.id.homeFragment, R.id.myCoursesFragment, R.id.balanceFragment, R.id.profileFragment
     )
 
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
